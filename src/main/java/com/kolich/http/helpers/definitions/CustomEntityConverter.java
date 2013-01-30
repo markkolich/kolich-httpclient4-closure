a76467337a0e6f304d46916807446f87d756bd30
Mark Kolich <social@kolich.com>
2013-01-30 09:21:53 -0800

    refactoring CustomEntityConverter a bit

--- src/main/java/com/kolich/http/helpers/definitions/CustomEntityConverter.java
+++ src/main/java/com/kolich/http/helpers/definitions/CustomEntityConverter.java
@@ -1,12 +1,6 @@
 package com.kolich.http.helpers.definitions;
 
-import com.kolich.http.HttpClient4Closure.HttpFailure;
-import com.kolich.http.HttpClient4Closure.HttpSuccess;
-
-public interface CustomEntityConverter<F,S> {
-
-	public S success(final HttpSuccess success) throws Exception;
-	
-	public F failure(final HttpFailure failure);
+public interface CustomEntityConverter<F,S> extends
+	CustomSuccessEntityConverter<S>, CustomFailureEntityConverter<F> {
 	
 }
